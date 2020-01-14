30c7fa14d57714748d8c2e1b4dac11a5cebbd17b
EknathUdhav.Take@idemia.com <1N@th@1202>
2020-01-14 17:06:39 +0530

    Unused classes removed

--- src/main/java/com/idemia/assetmanagement/model/asset/MacBook.java
+++
@@ -1,7 +0,0 @@
-package com.idemia.assetmanagement.model.asset;
-
-import lombok.Data;
-
-@Data
-public class MacBook extends Laptop {
-}
--- src/main/java/com/idemia/assetmanagement/model/asset/Windows.java
+++
@@ -1,7 +0,0 @@
-package com.idemia.assetmanagement.model.asset;
-
-import lombok.Data;
-
-@Data
-public class Windows extends Laptop {
-}
